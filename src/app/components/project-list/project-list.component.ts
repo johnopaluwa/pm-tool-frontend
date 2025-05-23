9288a9d815765977a6b87659a4cde5319e5532d7
John Opaluwa <johnopaluwa@gmail.com>
2025-05-23 04:52:41 +0200

    change naviagtion

--- src/app/components/project-list/project-list.component.ts
+++ src/app/components/project-list/project-list.component.ts
@@ -1,7 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { Subscription, forkJoin } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+import {
+  PredictionReview,
+  PredictionReviewService,
+} from '../../services/prediction-review.service'; // Import PredictionReviewService
 import { Project, ProjectService } from '../../services/project.service'; // Import ProjectService
 
 @Component({
@@ -13,19 +18,67 @@ import { Project, ProjectService } from '../../services/project.service'; // Imp
 })
 export class ProjectListComponent implements OnInit, OnDestroy {
   projects: Project[] = [];
-  private projectsSubscription: Subscription | undefined;
+  latestPredictionReviews: {
+    [projectId: string]: PredictionReview | undefined;
+  } = {};
+  private subscriptions: Subscription[] = [];
 
-  constructor(private projectService: ProjectService) {} // Inject ProjectService
+  constructor(
+    private projectService: ProjectService,
+    private predictionReviewService: PredictionReviewService // Inject PredictionReviewService
+  ) {}
 
   ngOnInit() {
-    this.projectsSubscription = this.projectService
+    const projectsSub = this.projectService
       .getProjects()
-      .subscribe((data) => {
-        this.projects = data;
+      .pipe(
+        switchMap((projects) => {
+          this.projects = projects;
+          if (projects.length === 0) {
+            return []; // Return empty array if no projects
+          }
+          // For each project, get its latest prediction review
+          const reviewObservables = projects.map((project) =>
+            this.predictionReviewService
+              .getPredictionReviewsByProjectId(project.id)
+              .pipe(
+                map((reviews) => {
+                  // Find the latest review for this project
+                  return reviews.sort(
+                    (a, b) =>
+                      new Date(b.generatedAt).getTime() -
+                      new Date(a.generatedAt).getTime()
+                  )[0];
+                })
+              )
+          );
+          return forkJoin(reviewObservables).pipe(
+            map((latestReviews) => {
+              // Map latest reviews to project IDs
+              const reviewsMap: {
+                [projectId: string]: PredictionReview | undefined;
+              } = {};
+              projects.forEach((project, index) => {
+                reviewsMap[project.id] = latestReviews[index];
+              });
+              return reviewsMap;
+            })
+          );
+        })
+      )
+      .subscribe((latestReviewsMap) => {
+        this.latestPredictionReviews = latestReviewsMap;
       });
+
+    this.subscriptions.push(projectsSub);
   }
 
   ngOnDestroy(): void {
-    this.projectsSubscription?.unsubscribe();
+    this.subscriptions.forEach((sub) => sub.unsubscribe());
+  }
+
+  hasPredictionRun(projectId: string): boolean {
+    const review = this.latestPredictionReviews[projectId];
+    return !!review; // Return true if a review exists, false otherwise
   }
 }
